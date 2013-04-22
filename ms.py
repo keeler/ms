0f887da33fdcd98b8956333e0caa4c60acbf0908
Keeler Russell <keeler.russell@gmail.com>
2013-04-22 12:38:42 -0500

    Added printing of overall statistics.

--- ms.py
+++ ms.py
@@ -2,11 +2,11 @@ import os
 from collections import defaultdict
 from optparse import OptionParser
 
-cols = ['pid', 'cmd', 'total', 'res', 'shr', 'uss', 'pss', 'swp']
+cols = ['pid', 'cmd', 'vsz', 'res', 'shr', 'uss', 'pss', 'swp']
 
 def read_smaps( pid ):
 	smaps = open( '/proc/' + str( pid ) + '/smaps' )
-	fieldmap = { 'Rss:' : 'res', 'Shared_Clean:' : 'shr', 'Shared_Dirty:' : 'shr', 'Private_Clean:' : 'uss', 'Private_Dirty:' : 'uss', 'Pss:' : 'pss', 'Size:' : 'total', 'Swap:' : 'swp' }
+	fieldmap = { 'Rss:' : 'res', 'Shared_Clean:' : 'shr', 'Shared_Dirty:' : 'shr', 'Private_Clean:' : 'uss', 'Private_Dirty:' : 'uss', 'Pss:' : 'pss', 'Size:' : 'vsz', 'Swap:' : 'swp' }
 	mem = defaultdict( int )
 	for line in smaps:
 		fields = line.split()
@@ -15,6 +15,18 @@ def read_smaps( pid ):
 
 	smaps.close()
 	return [mem[x] for x in cols[2:]]
+
+def get_overall_stats():
+	totalMem = 0
+	freeMem = 0
+	sharedMem = 0
+	for line in open( '/proc/meminfo' ):
+		fields = line.split()
+		if fields[0] == 'MemTotal:': totalMem = int( fields[1] )
+		elif fields[0] == 'MemFree:': freeMem = int( fields[1] )
+		elif fields[0] == 'Shmem:': sharedMem = int( fields[1] )
+
+	return [totalMem, freeMem, sharedMem]
 
 def get_processes():
 	procs = []
@@ -38,11 +50,26 @@ def print_processes( procs ):
 			print '|', str( p[i] ).rjust( maxes[cols[i]] ),
 		print '|\n',
 
+def print_overall_usage( procs ):
+	totalMem = 0
+	freeMem = 0
+	for line in open( '/proc/meminfo' ):
+		fields = line.split()
+		if fields[0] == 'MemTotal:': totalMem = int( fields[1] )
+		elif fields[0] == 'MemFree:': freeMem = int( fields[1] )
+
+	print 'Total:', totalMem, 'kB'
+	print 'Free:', freeMem, 'kB'
+	print 'In use:', totalMem - freeMem, 'kB'
+	sharedMem = sum( [x[cols.index( 'shr' )] for x in procs] )
+	print 'Shared:', sharedMem, 'kB'
+	print 'Non-shared:', totalMem - freeMem - sharedMem, 'kB'
+
 if __name__ == '__main__':
 	usage = 'usage: %prog [options]\nGet process memory statistics.'
 	parser = OptionParser( usage = usage )
 	parser.add_option( '-s', '--sort', metavar = 'STATISTIC',
-					   help = 'Sort entries based on a particular statistic. Available options are pid, cmd, res, shr, uss, pss, and swp. Append a + or - to sort ascending or descending order, respectively.' )
+					   help = 'Sort entries based on a particular statistic. Available options are pid, cmd, vsz, res, shr, uss, pss, and swp. Append a + or - to sort ascending or descending order, respectively.' )
 
 	( options, args ) = parser.parse_args()
 	if options.sort:
@@ -51,6 +78,7 @@ if __name__ == '__main__':
 			order = options.sort[-1]
 			procs = get_processes()
 			procs.sort( key = lambda x: x[cols.index( col )], reverse = False if order == '+' else True )
+			print_overall_usage( procs )
 			print_processes( procs )
 		else:
 			parser.print_help()
